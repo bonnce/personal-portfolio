76857757753cae7256a2ffc2a9affef533df62b3
bonnce <bonce_reloko@hotmail.com>
2023-10-27 19:48:37 -0300

    projects dynamic pages

---
+++ src/utils/projects_lists.ts
@@ -0,0 +1,37 @@
+export const projects = [
+	{
+		id:'messi1',
+		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		title:'messi',
+		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
+		alt:'messi chikito'
+	},
+	{
+		id:'messi2',
+		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		title:'messi',
+		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
+		alt:'messi chikito'
+	},
+	{
+		id:'messi3',
+		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		title:'messi',
+		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
+		alt:'messi chikito'
+	},
+	{
+		id:'messi4',
+		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		title:'messi',
+		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
+		alt:'messi chikito'
+	},
+	{
+		id:'messi5',
+		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		title:'messi',
+		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
+		alt:'messi chikito'
+	},
+]
