dda61b30ec6ad110f46a833d61d20549e2eb933a
bonnce <bonce_reloko@hotmail.com>
2023-11-06 18:50:09 -0300

    links updated, project individual done

--- src/utils/projects_lists.ts
+++ src/utils/projects_lists.ts
@@ -2,6 +2,10 @@ export const projects = [
 	{
 		id:'messi1',
 		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		descriptionExtended:`Este es messi, el mejor jugador del planeta tierra, normalmente esta quieto sin hacer nada para desconcentrar
+		al rival pero en ciertos momentos activa su ultra instinct, que se vio reducido el tiempo de uso a medida que se fue volviendo
+		mas viejo, y traspasa a todos los rivales y adquiere un 90% de efectividad, la seleccion argentina, se basa estrictamente
+		en correr por el y aguantar hasta que el vea oportuno usar su habilidad.`,
 		title:'messi',
 		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
 		alt:'messi chikito'
@@ -9,6 +13,7 @@ export const projects = [
 	{
 		id:'messi2',
 		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		descriptionExtended:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
 		title:'messi',
 		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
 		alt:'messi chikito'
@@ -16,6 +21,7 @@ export const projects = [
 	{
 		id:'messi3',
 		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		descriptionExtended:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
 		title:'messi',
 		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
 		alt:'messi chikito'
@@ -23,6 +29,7 @@ export const projects = [
 	{
 		id:'messi4',
 		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		descriptionExtended:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
 		title:'messi',
 		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
 		alt:'messi chikito'
@@ -30,6 +37,7 @@ export const projects = [
 	{
 		id:'messi5',
 		description:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
+		descriptionExtended:"Hola soy messi el mejor del mundo y te mando un fuerte abrazo pa'",
 		title:'messi',
 		img:'https://pbs.twimg.com/media/FkbNNUYXkAMIn3F.jpg',
 		alt:'messi chikito'
