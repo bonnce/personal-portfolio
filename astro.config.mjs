700725f113776182488f6ec7ccd65c47131844cd
bonnce <bonce_reloko@hotmail.com>
2023-12-01 13:16:50 -0300

    vercel deployment v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'hybrid',
-  integrations: [preact(), tailwind()]
+  output: 'hybrid',
+  integrations: [preact(), tailwind()],
+  adapter: vercel()
 });
